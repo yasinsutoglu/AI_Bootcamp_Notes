a2c430aaae311f286c9dd73d6e3470e0dbfc5a89
Yasin Sutoglu <ysutoglu@gmail.com>
2024-02-22 10:58:27 +0300

    user-based collaborative filtering

--- recommender_systems/content_based_recommender/content_based_recommender.py
+++ recommender_systems/content_based_recommender/content_based_recommender.py
@@ -18,11 +18,13 @@
 #################################
 
 import pandas as pd
+import numpy as np
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 pd.set_option('display.expand_frame_repr', False)
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import csr_matrix
 # https://www.kaggle.com/rounakbanik/the-movies-dataset
 df = pd.read_csv("section_datasets/recommender/datasets/the_movies_dataset/movies_metadata.csv", low_memory=False)
 # DtypeWarning kapamak icin
@@ -43,29 +45,44 @@ tfidf_matrix.shape #(45466, 75827) => ilki yorumlar, ikincisi unique kelimeler
 df['title'].shape
 
 tfidf.get_feature_names_out()
-tfidf.get_feature_names()
+#tfidf.get_feature_names()
 
-tfidf_matrix.toarray()
-
+csr_tfidf_matrix = csr_matrix(tfidf_matrix, dtype=np.float32)
+# tfidf_matrix.toarray() => Unable to allocate 25.7 GiB for an array with shape (45466, 75827)
+# and data type float64 için memoryError verdi ve ben de "from scipy.sparse import csr_matrix" kullandım
+csr_tfidf_matrix.toarray()
 
 #################################
 # 2. Cosine Similarity Matrisinin Oluşturulması
 #################################
 
-cosine_sim = cosine_similarity(tfidf_matrix,
-                               tfidf_matrix)
+# diğer benzerlik/uzaklık ölçüleri => manhattan distance, öklid distance, pearson correlation etc.
+# durumdan duruma göre hangisini kullanacağımın tercihi değişir.
+cosine_sim = cosine_similarity(csr_tfidf_matrix,
+                               csr_tfidf_matrix)
 
 cosine_sim.shape
-cosine_sim[1]
-
+# (45466, 45466) => 45466: overview'ların sayısıdır
+cosine_sim[1] # bu 1nci indexteki filmin diğer tüm filmlerle benzerlik score'ları var
 
 #################################
 # 3. Benzerliklere Göre Önerilerin Yapılması
 #################################
 
+# pandas series olusturup dataframe index sütunu ve title sütunu index bilgilerini verdik
 indices = pd.Series(df.index, index=df['title'])
 
 indices.index.value_counts()
+#title
+#Cinderella              11
+#Hamlet                   9
+#Alice in Wonderland      9
+#Beauty and the Beast     8
+#Les Misérables           8
+#                       ..
+# Burada bazı titlelar birden fazla matriste yer aldığı için teke düşürmemiz gerekir.
+# Bu bir sorundur.Bu tip çoklanan isimlendirmelerin en sonuncusu alınır.
+# duplicated(keep='first') default'tur. True/ false döner. False => duplicate olmayandır.
 
 indices = indices[~indices.index.duplicated(keep='last')]
 
@@ -75,12 +92,13 @@ indices["Sherlock Holmes"]
 
 movie_index = indices["Sherlock Holmes"]
 
-cosine_sim[movie_index]
+cosine_sim[movie_index] # "Sherlock Holmes" filmi ile diğer filmlerin similarity'leri ölçüldü
 
 similarity_scores = pd.DataFrame(cosine_sim[movie_index],
                                  columns=["score"])
 
 movie_indices = similarity_scores.sort_values("score", ascending=False)[1:11].index
+# 0ncı indexte kendi filmimiz var oldugu için atladık ve [1:11] yaptık
 
 df['title'].iloc[movie_indices]
 
@@ -119,6 +137,8 @@ def calculate_cosine_sim(dataframe):
 
 cosine_sim = calculate_cosine_sim(df)
 content_based_recommender('The Dark Knight Rises', cosine_sim, df)
+# Bu işlemleri webSitemizdeki en çok izlenen 100 filmi çekip her film için öneri listesi olusturup SQL tablosuna gömeriz
+# Kullanıcı bu 100 filmden birini izleyince tak otomatik tablodan çekip onune basarız.
 # 1 [90, 12, 23, 45, 67]
 # 2 [90, 12, 23, 45, 67]
 # 3
--- recommender_systems/item_based_recommender/item_based_recommender.py
+++ recommender_systems/item_based_recommender/item_based_recommender.py
@@ -9,36 +9,55 @@
 # Adım 3: Item-Based Film Önerilerinin Yapılması
 # Adım 4: Çalışma Scriptinin Hazırlanması
 
+
+
 ######################################
 # Adım 1: Veri Setinin Hazırlanması
 ######################################
 import pandas as pd
 pd.set_option('display.max_columns', 500)
-movie = pd.read_csv('datasets/movie_lens_dataset/movie.csv')
-rating = pd.read_csv('datasets/movie_lens_dataset/rating.csv')
-df = movie.merge(rating, how="left", on="movieId")
+movie = pd.read_csv('section_datasets/recommender/datasets/movie_lens_dataset/movie.csv')
+rating = pd.read_csv('section_datasets/recommender/datasets/movie_lens_dataset/rating.csv')
+df = movie.merge(rating, how="left", on="movieId") # SQL'deki JOIN işlemi yaptık.
 df.head()
 
 
 ######################################
 # Adım 2: User Movie Df'inin Oluşturulması
 ######################################
-
+# Örneğin 3 filmden biri 1000 rating , diğeri 40 rating, öbürü 3 rating almış olsun
+# Biz bu az rating alanları hesaba katarsak hesaplama maliyetleri külfetli olur
+# Dolayısıyla bu tarz filmleri burada eleme işlemi yapıyoruz.
 df.head()
 df.shape
 
 df["title"].nunique()
 
-df["title"].value_counts().head()
+df["title"].value_counts().head() # hangi filme kacar yorum gelmiş
 
 comment_counts = pd.DataFrame(df["title"].value_counts())
-rare_movies = comment_counts[comment_counts["title"] <= 1000].index
+#                                           count
+#title
+#Pulp Fiction (1994)                        67310
+#Forrest Gump (1994)                        66172
+#Shawshank Redemption, The (1994)           63366
+#Silence of the Lambs, The (1991)           63299
+
+rare_movies = comment_counts[comment_counts["count"] <= 1000].index
 common_movies = df[~df["title"].isin(rare_movies)]
-common_movies.shape
-common_movies["title"].nunique()
+common_movies.shape # (17766015, 6)
+
+common_movies["title"].nunique() #3159 tane film 17766015 tane rating almış.
 df["title"].nunique()
 
 user_movie_df = common_movies.pivot_table(index=["userId"], columns=["title"], values="rating")
+# DÜZELTME HATA ALMAMAK İÇİN
+#common_movies = common_movies.dropna()
+#common_movies_cleaned = common_movies.dropna().drop_duplicates(["userId", "title"])
+#common_movies_cleaned.loc[:, "rating"] = common_movies_cleaned["rating"].astype("float32", copy=False)
+#common_movies_cleaned.loc[:, "userId"] = common_movies_cleaned["userId"].astype(str,errors='ignore',copy=False)
+#common_movies_cleaned.loc[:, "userId"] = common_movies_cleaned["userId"].str.split(".", expand=True)[0]
+#user_movie_df = common_movies_cleaned.pivot_table(index="userId", columns="title", values="rating")
 
 user_movie_df.shape
 user_movie_df.columns
@@ -51,13 +70,13 @@ user_movie_df.columns
 movie_name = "Matrix, The (1999)"
 movie_name = "Ocean's Twelve (2004)"
 movie_name = user_movie_df[movie_name]
+# corrwith() => korrelasyona bakma fonksiyonu
 user_movie_df.corrwith(movie_name).sort_values(ascending=False).head(10)
 
-
+## RASTGELE BİR FILM SECIP ONUNLA CORRELATION'a bakma
 movie_name = pd.Series(user_movie_df.columns).sample(1).values[0]
 movie_name = user_movie_df[movie_name]
 user_movie_df.corrwith(movie_name).sort_values(ascending=False).head(10)
-
 
 def check_film(keyword, user_movie_df):
     return [col for col in user_movie_df.columns if keyword in col]
