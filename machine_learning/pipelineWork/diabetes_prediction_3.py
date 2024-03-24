7c39cd4cb523e2fed77d999a194bc40c99951f50
Yasin Sutoglu <ysutoglu@gmail.com>
2024-03-24 12:24:32 +0300

    DB güncellemesi sonrası pipeline

--- machine_learning/pipelineWork/diabetes_prediction_3.py
+++ machine_learning/pipelineWork/diabetes_prediction_3.py
@@ -5,14 +5,16 @@
 import joblib
 import pandas as pd
 
+# yeni veri girişlerinin olduğu DB'imiz
 df = pd.read_csv("datasets/diabetes.csv")
 
 random_user = df.sample(1, random_state=45)
 
 new_model = joblib.load("voting_clf.pkl")
 
-new_model.predict(random_user)
+new_model.predict(random_user) # yeni veri girişi sonrası hata alabiliriz
 
+# Yeni veri gelmesi sonrası tekrar data-preprocessing ihtiyacı olur
 from AI_Bootcamp_Sections.machine_learning.pipelineWork.diabetes_pipeline_2 import diabetes_data_prep
 
 X, y = diabetes_data_prep(df)
